c402eaab0ee59cbd5a50e7291ffb3b122c39672a
sushilibdev <aegissvarnacorporation@gmail.com>
2026-02-28 14:19:10 +0700

    change import in test file

--- src/fetcher.test.ts
+++ src/fetcher.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest'
-import { createSushi, sushiFetch } from './fetcher'
+import { createSushi, sushiFetch } from './core/fetcher'
 
 // Mock globalThis.fetch
 globalThis.fetch = vi.fn() as any
